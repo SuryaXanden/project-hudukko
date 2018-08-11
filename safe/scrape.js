1a690429cf50d4826c66d48e5976781eb482e2f0
Surya Xanden <26842395+SuryaXanden@users.noreply.github.com>
2018-08-11 23:56:20 +0530

    Add files via upload

---
+++ safe/scrape.js
@@ -0,0 +1,42 @@
+//Generating the URL
+let url = 'https://www.amazon.in/s/ref=sr_st_price-asc-rank?keywords=';
+let qi=qp='';
+process.argv.forEach((val, index, array) =>
+{
+	if(index==2)
+			qi = val;
+	if(index==3)
+			qp = val;
+});
+//set url
+url+=qi+" "+qp;
+
+//importing library
+const request = require('request');
+const cheerio = require('cheerio');
+
+request(url,(error,response,html)=>{
+  if(!error && response.statusCode === 200)
+  {
+    const $ = cheerio.load(html);
+    // let id = $('#s-results-list-atf');//html() or text() is working
+    let limit = 6;
+	for(let itr=0;itr<limit;itr++)
+	{
+		//console.log("Iteration "+(itr+1)+": "+itr+"<br>");
+		//let link = $(el).attr('href');//for links
+		let id = $('#result_'+itr+"").each((i,el) =>
+		{
+			//if not sponsored
+			if($(el).html().match(/^((?!\[Sponsored\]|Sponsored).)*$/img))
+			{
+				console.log("<br>Image "+itr+"= "+$(el).find("img").attr("src"));
+				
+			
+			
+			//only on text .replace(/\s\s+/img,"")
+			
+		});
+	}
+  }
+});
