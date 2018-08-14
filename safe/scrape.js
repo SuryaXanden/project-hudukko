c399e4adba7975cb4924807a8a168a256c03a9bb
Surya Xanden <SuryaXanden@github.com>
2018-08-15 03:03:17 +0530

    Amazon & Snapdeal

--- safe/scrape.js
+++ safe/scrape.js
@@ -7,8 +7,6 @@ process.argv.forEach((val, index, array) =>
 	if(index==3)
 		if(val!=0)
 			qp = val;
-		else
-			qp = 0;
 });
 const request = require('request');
 const cheerio = require('cheerio');
@@ -88,8 +86,88 @@ function amazon()
 	});
 }
 
+function snapdeal()
+{
+	let urls = 'https://www.snapdeal.com/search?sort=rlvncy&keyword=';
+
+	request(urls+inj,(error,response,html)=>
+	{
+		if(!error && response.statusCode === 200)
+		{
+			const $ = cheerio.load(html);
+			
+			//image
+			var img = $("#products").map(function()
+			{
+				return $(this).find("img").map(function()
+				{
+					return $(this).attr("src");
+				}).get();        
+			}).get();
+			
+			//title
+			var tit = $("#products").map(function()
+			{
+				return $(this).find("img").map(function()
+				{
+					return $(this).attr("title");
+				}).get();        
+			}).get();
+			
+			//urls
+			var links = $("#products").map(function()
+			{
+				return $(this).find("a").map(function()
+				{
+					return $(this).attr("href");
+				}).get();        
+			}).get();
+				
+			//price
+			var price = $("#products").map(function()
+			{
+				return $(this).find("span").map(function()
+				{
+					return $(this).attr("data-price");
+				}).get();        
+			}).get();
+				
+			var link = new Set(links);
+			
+			var getEntriesArry = link.entries();
+			
+			let dmp = [];
+			let l = String(getEntriesArry.next().value);		
+				l = l.substring(0,l.indexOf(","));
+			dmp.push(l);
+			 l = String(getEntriesArry.next().value);		
+				l = l.substring(0,l.indexOf(","));
+			dmp.push(l);
+			 l = String(getEntriesArry.next().value);		
+				l = l.substring(0,l.indexOf(","));
+			dmp.push(l);
+			 l = String(getEntriesArry.next().value);		
+				l = l.substring(0,l.indexOf(","));
+			dmp.push(l);
+			
+			let retstr = "";
+			for(var i=0; i<4; i++)
+			{
+				if(price[i]<=qp*1.15 && price[i]>=0)
+				{
+					retstr = "<div id=\"results\" class=\"row\" style=\"width:100%;\">";
+						retstr += "<div class=\"col-xs-3\" style=\"background-color:rgba(1,1,1,30%);\">";
+							retstr += "<a href=\""+dmp[i]+"\">";//url				
+								retstr += "<img src=\""+img[i]+"\" style=\"width:128px;border-radius:32px;height:128px;\">";//image			  
+								retstr += "<br><span id=\"title\" style=\"float:left\">"+String(tit[i]).substring(0,11)+"...</span>";//title
+								retstr += "<span id=\"price\" style=\"float:right\">Rs&Tab;"+price[i]+"</span>";
+					retstr += "</a></div>";
+					console.log(retstr);
+				}
+			}				
+		}
+	});
+}
+
+snapdeal();
 amazon();
-
-
-
-console.log("</div>");
